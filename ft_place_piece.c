52aea94f3dd15d33b68e4aa0f16e81095a768af5
Sandra <sgury@student.42.fr>
2019-06-24 15:34:14 +0200

    strategy not working

--- ft_place_piece.c
+++ ft_place_piece.c
@@ -6,73 +6,79 @@
 /*   By: sgury <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/16 10:15:42 by sgury             #+#    #+#             */
-/*   Updated: 2019/06/24 14:35:40 by sgury            ###   ########.fr       */
+/*   Updated: 2019/06/24 15:32:14 by sgury            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include	"filler.h"
 
-int		ft_place_piece(t_map *map, t_piece *piece)
+static void	send_solution(int x, int y)
 {
-	int		x;
-	int		y;
-	int		i;
-	int		j;
-	int		common;
-//	int		score;
-	int		badopt;
 	char	solution[50];
 
-	x = 0;
-	y = 0;
+	ft_bzero(solution, sizeof(solution));
+	ft_strcat(solution, ft_itoa(x));
+	ft_strcat(solution, " ");
+	ft_strcat(solution, ft_itoa(y));
+	ft_strcat(solution, "\n");
+	ft_putstr_fd(solution, 1);
+}
+
+static void get_score(t_map *map, t_piece *piece, t_solution *solution)
+{
+	int	i;
+	int	j;
+	int	badspot;
+	int touch;
+	int	new_score;
+
 	i = 0;
 	j = 0;
-	common = 0;
-//	score = 0;
-	ft_bzero(solution, sizeof(solution));
-	while (x + piece->x < map->x)
+	badspot = 0;
+	new_score = 0;
+	touch = 0;
+	while (i < piece->x)
 	{
-		while (y + piece->y < map->y)
+		while (j < piece->y)
 		{
-			while (i < piece->x)
+			if (piece->piece[i][j] == '*' && map->map[solution->i + i][solution->j + j] == map->enemy)
 			{
-				while (j < piece->y)
-				{
-					if (piece->piece[i][j] == '*' && map->map[x + i][y + j] == map->enemy)
-					{
-						badopt = 1;
-						break;
-					}
-					if (piece->piece[i][j] == '*' && map->map[x + i][y + j] == map->player)
-						common++;
-//					if (piece->piece[i][j] == '*' && ft_isdigit(map->map[x + i][y + j]))
-//						score += map->map[x + i][y + j] - '0';
-					j++;
-				}
-				if (badopt == 1)
-					break;
-				j = 0;
-				i++;
+				badspot = 1;
+				break;
 			}
-			if (common == 1 && badopt == 0)
-			{
-				ft_strcat(solution, ft_itoa(x));
-				ft_strcat(solution, " ");
-				ft_strcat(solution, ft_itoa(y));
-				ft_strcat(solution, "\n");
-				ft_putstr_fd(solution, 1);
-				return (1);
-			}
-			badopt = 0;
-			common = 0;
-//			score = 0;
-			i = 0;
-			j = 0;
-			y++;
+			if (piece->piece[i][j] == '*' && map->map[solution->i + i][solution->j + j] == map->player)
+				touch++;
+			if (piece->piece[i][j] == '*' && ft_isdigit(map->map[solution->i + i][solution->j + j]))
+				new_score += map->map[solution->i + i][solution->j + j] - '0';
+			j++;
 		}
-		y = 0;
-		x++;
+		if (badspot)
+			break;
+		j = 0;
+		i++;
 	}
-	ft_putstr_fd("00 00\n", 1);
-	return (-1);
+	if ((new_score < solution->score || solution->score == 0) && touch == 1 && badspot == 0)
+	{
+		solution->x = solution->i;
+		solution->y = solution->j;
+		solution->score = new_score;
+	}
+}
+
+void		ft_place_piece(t_map *map, t_piece *piece)
+{
+	t_solution	solution;
+
+	ft_bzero(&solution, sizeof(t_solution));
+	while (solution.i + piece->x < map->x)
+	{
+		while (solution.j + piece->y < map->y)
+		{
+			get_score(map, piece, &solution);
+			solution.j++;
+		}
+		solution.j = 0;
+		solution.i++;
+	}
+	send_solution(solution.x, solution.y);
 }
